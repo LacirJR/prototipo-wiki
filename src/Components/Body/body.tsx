faf14e1adc092e194b4fabfcbcd5c2a8059b87ec
Lacir Junior <lacir.junior@outlook.com>
2022-12-24 17:43:29 -0300

    ajuste imagem

--- src/Components/Body/body.tsx
+++ src/Components/Body/body.tsx
@@ -9,7 +9,7 @@ type PropsButton = {
 const BarraDePesquisa = () => {
     return (
         <div className='searchBox'>
-         <img className='imgSearch' src='../img/search-solid.svg'/>
+         <img className='imgSearch' src='https://raw.githubusercontent.com/LacirJR/prototipo-wiki/e822fe367b962579b14f4603833928b3fdabeb6b/img/search-solid.svg'/>
          <input type={'text'} id='searchPost' placeholder='Pesquisar'/>
         </div>
     )
